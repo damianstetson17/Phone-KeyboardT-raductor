c1d3055836f71461da4d5537470f03f9c412f666
Damian Stetson <damianstetson17@gmail.com>
2021-05-20 00:09:38 -0300

    first commit

---
+++ Controller/TextController.cs
@@ -0,0 +1,112 @@
+using System;
+using System.Collections.Generic;
+
+namespace Calculator.Controllers
+{
+    /*  
+        The TextController class
+        This class contains all necessary methods for process a text input
+    */
+    public class TextController
+    {
+        /*
+            This method return a string of numerical coordinates from a word
+        */
+        public string getNumberSecuence(string word){
+            string resultCoord ="";
+            foreach (char letter in word)
+            {
+                //if its the first input
+                if(resultCoord.Length == 0){
+                    resultCoord += getLetterNumber(letter);
+                }else{
+                    //getting the next number coords
+                    string nextNumbers = getLetterNumber(letter);
+                    //it if the last number in input is equal to the first new one
+                    if(resultCoord[resultCoord.Length-1] == nextNumbers[0])
+                    {
+                        resultCoord += " " + nextNumbers;
+                    }else{
+                        resultCoord += nextNumbers;
+                    }
+                }  
+            }
+            return (resultCoord);
+        }
+
+        /*
+            This method return a string of numerica coordinate from a char
+        */
+        public string getLetterNumber(char OneLetter){
+            OneLetter = Char.ToLower(OneLetter);
+            int lettersInButtonLimit = 3;
+            int MaybeIsNextButton = 1;
+            char charOffset;
+            char charLimit;
+            string charCoord ="";
+            int button = 2;
+
+            if(OneLetter != ' ')
+            {   
+                //if the letter is on button 7 or 8 or 9 button
+                if(OneLetter > 'o')
+                {
+                    charOffset = 'p';
+                    //if the letter is on button 7
+                    if(OneLetter <'t')
+                    {
+                        charLimit = 't';
+                        button = 7;
+                        lettersInButtonLimit = 4 ;   
+                    }else{
+                        //if the letter is on button 8 
+                        if(OneLetter <'w')
+                        {
+                            charLimit = 'w';
+                            button = 8;
+                            lettersInButtonLimit = 3 ;
+                        }else{
+                            //the letter is on button 9
+                            charOffset = 'w';
+                            charLimit = 'z';
+                            button = 9;
+                            lettersInButtonLimit = 4 ; 
+                        }
+                    }
+                }else{
+                    charOffset = 'a';
+                    charLimit = 'o';
+                    lettersInButtonLimit = 3 ;
+                }
+                //if its not a 'space' letter
+                for (char l = charOffset; l <= charLimit; l++)
+                {
+                    //if its not the letter... 
+                    if(OneLetter != l)
+                    {
+                        //if we go over three letters, we must continue through the next botton and count again...
+                        if(MaybeIsNextButton >= lettersInButtonLimit)
+                        {
+                            button++;
+                            MaybeIsNextButton = 1;
+                        }else{
+                            MaybeIsNextButton++;
+                        }
+                    //if its the letter!!
+                    }else{
+                        charCoord = button.ToString();
+                        //add the necessary repetitions in button for get the letter
+                        for (int i = 1; i < MaybeIsNextButton; i++)
+                        {
+                            charCoord += button.ToString();
+                        }
+                        break;
+                    }
+                }
+            }else{
+                charCoord = "0";
+            }
+            return (charCoord);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using Calculator.Controllers;
+using System;
+
+namespace Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Controller
+            TextController textController = new TextController();
+            
+            //Input
+            Boolean option = true;
+            while(option)
+            {
+                Console.WriteLine("============[Bienvenido!]============");
+                Console.WriteLine("[ Por favor, a continuación ingrese la\n palabra que desea convertir! ]");
+
+                string resultCoords = textController.getNumberSecuence(Console.ReadLine());
+
+                Console.WriteLine($"Tus cordenadas numéricas serán: {resultCoords}");
+                Console.WriteLine("[ ¿Desee cargar otra palabra? (y/n) ]");
+                Console.WriteLine("==================[]=================");
+                option =  Console.ReadLine() == "n" ? false : true;
+                Console.Clear();
+                Console.WriteLine(". . .Adiós!");
+            }
+
+        }
+    }
+}
